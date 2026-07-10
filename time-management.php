9b06d7f57657b7590c328ec9df5b2d26e9ee2c6a
VINIX-10 <alvinnurfaiz01@gmail.com>
2026-07-10 10:10:07 +0700

    membuat php semakin simple dengan proses belakang

--- time-management.php
+++ time-management.php
@@ -1,31 +1,4 @@
-<?php
-require 'sesi.php';
-$pesan_alert = "";
-
-if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['simpan_logbook'])) {
-    $id_kehadiran = $_POST['id_kehadiran'];
-    $catatan_kerja = $_POST['catatan_kerja'];
-    $stmt = $conn->prepare("UPDATE kehadiran SET catatan = ? WHERE id = ? AND user_id = ?");
-    $stmt->bind_param("sii", $catatan_kerja, $id_kehadiran, $user_id);
-    if ($stmt->execute()) {
-        $pesan_alert = "Logbook aktivitas berhasil diperbarui!";
-    } else {
-        $pesan_alert = "Gagal memperbarui logbook.";
-    }
-}
-
-$query_riwayat = $conn->query("SELECT * FROM kehadiran WHERE user_id = '$user_id' ORDER BY tanggal DESC");
-
-function hariIndo(string $tanggal) {
-    $hari_inggris = date('l', strtotime($tanggal));
-    $daftar_hari = [
-        'Sunday' => 'Minggu', 'Monday' => 'Senin', 'Tuesday' => 'Selasa',
-        'Wednesday' => 'Rabu', 'Thursday' => 'Kamis', 'Friday' => 'Jumat', 'Saturday' => 'Sabtu'
-    ];
-    return $daftar_hari[$hari_inggris];
-}
-?>
-
+<?php require 'proses_time_management.php'; ?>
 <!DOCTYPE html>
 <html lang="id">
 <head>
